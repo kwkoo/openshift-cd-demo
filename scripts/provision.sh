766f8481924acf5fbbbf57ec452608650f6adaa6
Kin-Wai Koo <glug71@gmail.com>
2019-11-19 19:07:37 +0800

    Bring functionality of persistent deployment up to functionality of ephemeral deployment

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -170,7 +170,7 @@ function deploy() {
 
   sleep 2
 
-  local template=${BASE}/../cicd-template.yaml
+  local template=${BASE}/../yaml/cicd-template.yaml
   echo "Using template $template"
   oc $ARG_OC_OPS new-app -f $template -p DEV_PROJECT=dev-$PRJ_SUFFIX -p STAGE_PROJECT=stage-$PRJ_SUFFIX -p EPHEMERAL=$ARG_EPHEMERAL -p ENABLE_QUAY=$ARG_ENABLE_QUAY -p QUAY_USERNAME=$ARG_QUAY_USER -p QUAY_PASSWORD=$ARG_QUAY_PASS -n cicd-$PRJ_SUFFIX 
 }
